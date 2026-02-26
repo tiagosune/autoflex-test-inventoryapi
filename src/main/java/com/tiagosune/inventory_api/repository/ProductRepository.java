a249d665ea0fcaffd9a7f4c01369df553e4215ed
Tiago <tiagosune1@hotmail.com>
2026-02-26 00:40:44 -0300

    feat: add `ProductRepository` interface for JPA operations on `Product` entity

---
+++ src/main/java/com/tiagosune/inventory_api/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.tiagosune.inventory_api.repository;
+
+import com.tiagosune.inventory_api.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository <Product, Long>{
+}
