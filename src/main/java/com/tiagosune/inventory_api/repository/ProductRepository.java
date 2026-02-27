cbeb7319f0ca4dae72763a6c4c71b1fc2430ec72
Tiago <tiagosune1@hotmail.com>
2026-02-26 23:10:22 -0300

    feat: add `existsByCode` method to `ProductRepository`

--- src/main/java/com/tiagosune/inventory_api/repository/ProductRepository.java
+++ src/main/java/com/tiagosune/inventory_api/repository/ProductRepository.java
@@ -4,4 +4,7 @@ import com.tiagosune.inventory_api.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository <Product, Long>{
+
+
+    boolean existsByCode(String code);
 }
