069ef9a4855b670e0730609b1ec714ed2749f909
Tiago <tiagosune1@hotmail.com>
2026-02-27 02:36:36 -0300

    feat: implement core CRUD operations for `ProductService`
    - Add `createProduct`, `updateProduct`, `deleteProduct`, `findProductById`, and `findAllProducts` methods.
    - Include raw material validation and mapping in product creation and updates.

--- src/main/java/com/tiagosune/inventory_api/service/ProductService.java
+++ src/main/java/com/tiagosune/inventory_api/service/ProductService.java
@@ -1,11 +1,98 @@
 package com.tiagosune.inventory_api.service;
 
+import com.tiagosune.inventory_api.dto.product.*;
+import com.tiagosune.inventory_api.entity.Product;
+import com.tiagosune.inventory_api.entity.ProductRawMaterial;
+import com.tiagosune.inventory_api.entity.RawMaterial;
+import com.tiagosune.inventory_api.exception.BusinessException;
+import com.tiagosune.inventory_api.exception.ResourceNotFoundException;
+import com.tiagosune.inventory_api.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
+
+    private final ProductRepository repository;
+    private final RawMaterialService rawMaterialService;
+
+    public ProductResponse createProduct (ProductRequest request) {
+
+        validateRawMaterials(request.getRawMaterials());
+        validateDuplicatedRawMaterials(request.getRawMaterials());
+        validateUniqueCode(request.getCode());
+        validateName(request.getName());
+        validatePrice(request.getPrice());
+
+        Product product = Product.builder()
+                .code(request.getCode())
+                .name(request.getName())
+                .price(request.getPrice())
+                .build();
+
+        List<ProductRawMaterial> compositions = new ArrayList<>();
+
+        for (ProductRawMaterialRequest item : request.getRawMaterials()) {
+            RawMaterial rawMaterial = rawMaterialService.findEntityByIdOrThrow(item.getRawMaterialId());
+
+            if (item.getRequiredQuantity().compareTo(BigDecimal.ZERO) <= 0) {
+                throw new BusinessException("Required quantity must be greater than zero");
+            }
+
+            ProductRawMaterial prm = ProductRawMaterial.builder()
+                    .product(product)
+                    .rawMaterial(rawMaterial)
+                    .requiredQuantity(item.getRequiredQuantity())
+                    .build();
+
+            compositions.add(prm);
+        }
+
+        product.setRawMaterials(compositions);
+
+        repository.save(product);
+
+        return mapToResponse(product);
+    }
+
+    public ProductResponse updateProduct (Long id, ProductUpdateRequest request) {
+        Product product = findEntityById(id);
+
+        validateName(request.getName());
+        validatePrice(request.getPrice());
+
+        product.setName(request.getName());
+        product.setPrice(request.getPrice());
+
+        repository.save(product);
+
+        return mapToResponse(product);
+    }
+
+    public void deleteProduct (Long id) {
+        Product product = findEntityById(id);
+        repository.delete(product);
+    }
+
+    public ProductResponse findProductById (Long id) {
+        Product product = findEntityById(id);
+
+        return mapToResponse(product);
+    }
+
+    public List<ProductResponse> findAllProducts () {
+        return repository.findAll().stream()
+                .map(this::mapToResponse)
+                .toList();
+    }
+
     //private methods
 
     private Product findEntityById (Long id) {
@@ -35,6 +122,28 @@ public class ProductService {
         if (rawMaterial == null || rawMaterial.isEmpty()) {
             throw new BusinessException("Raw materials cannot be empty");
         }
+    }
+
+    private ProductResponse mapToResponse(Product product) {
+
+        List<ProductRawMaterialResponse> compositions =
+                product.getRawMaterials()
+                        .stream()
+                        .map(prm -> ProductRawMaterialResponse.builder()
+                                .rawMaterialId(prm.getRawMaterial().getId())
+                                .rawMaterialName(prm.getRawMaterial().getName())
+                                .requiredQuantity(prm.getRequiredQuantity())
+                                .build()
+                        )
+                        .toList();
+
+        return ProductResponse.builder()
+                .id(product.getId())
+                .code(product.getCode())
+                .name(product.getName())
+                .price(product.getPrice())
+                .rawMaterials(compositions)
+                .build();
     }
 
     private void validateDuplicatedRawMaterials (List<ProductRawMaterialRequest> rawMaterialIds) {
