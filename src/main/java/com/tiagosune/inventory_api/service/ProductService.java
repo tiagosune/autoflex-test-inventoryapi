4e8637a94465cc96ed2c342550aabae4a2bbe256
Tiago <tiagosune1@hotmail.com>
2026-02-27 02:35:09 -0300

    feat: add validation methods to `ProductService`
    - Implement private methods to validate product attributes:
      - Unique code, name, price, raw materials existence, and uniqueness.
    - Enhance input validation for product creation and updates.

--- src/main/java/com/tiagosune/inventory_api/service/ProductService.java
+++ src/main/java/com/tiagosune/inventory_api/service/ProductService.java
@@ -6,4 +6,46 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class ProductService {
+    //private methods
+
+    private Product findEntityById (Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Product not found"));
+    }
+
+    private void validateUniqueCode (String code) {
+        if (repository.existsByCode(code)) {
+            throw new BusinessException("Code already exists");
+        }
+    }
+
+    private void validateName (String name) {
+        if (name.isBlank()) {
+            throw new BusinessException("Name cannot be blank");
+        }
+    }
+
+    private void validatePrice (BigDecimal price) {
+        if (price.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new BusinessException("Price cannot be negative");
+        }
+    }
+
+    private void validateRawMaterials (List<ProductRawMaterialRequest> rawMaterial) {
+        if (rawMaterial == null || rawMaterial.isEmpty()) {
+            throw new BusinessException("Raw materials cannot be empty");
+        }
+    }
+
+    private void validateDuplicatedRawMaterials (List<ProductRawMaterialRequest> rawMaterialIds) {
+        List<Long> ids = rawMaterialIds.stream()
+                .map(ProductRawMaterialRequest::getRawMaterialId)
+                .toList();
+
+        Set<Long> uniqueIds = new HashSet<>(ids);
+
+        if (uniqueIds.size() != ids.size()) {
+            throw new BusinessException("Raw materials must be unique");
+        }
+    }
 }
