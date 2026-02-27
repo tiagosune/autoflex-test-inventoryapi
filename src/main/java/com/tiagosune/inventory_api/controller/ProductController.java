899d6f99845db1134207b88ec68757d78d2f7c64
Tiago <tiagosune1@hotmail.com>
2026-02-27 17:52:31 -0300

    feat: add CRUD endpoints for `ProductController`
    - Implement core CRUD operations: create, update, delete, and retrieve product details.
    - Add endpoints for managing raw materials (add, update quantity, remove) within products.
    - Include list retrieval for all products with validation.

--- src/main/java/com/tiagosune/inventory_api/controller/ProductController.java
+++ src/main/java/com/tiagosune/inventory_api/controller/ProductController.java
@@ -1,11 +1,63 @@
 package com.tiagosune.inventory_api.controller;
 
+import com.tiagosune.inventory_api.dto.product.*;
+import com.tiagosune.inventory_api.service.ProductService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
 @RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<ProductResponse> createProduct(@Valid @RequestBody ProductRequest request) {
+        ProductResponse response = productService.createProduct(request);
+
+        return ResponseEntity.created(URI.create("/api/products/" + response.getId())).body(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductResponse> updateBasicData (@PathVariable Long id, @Valid @RequestBody ProductUpdateRequest request) {
+        return ResponseEntity.ok(productService.updateProductBasicData(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct (@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> getProductById (@PathVariable Long id) {
+        ProductResponse response = productService.findProductById(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponse>> getAllProducts () {
+        return ResponseEntity.ok(productService.findAllProducts());
+    }
+
+    @PostMapping("/{id}/raw-materials")
+    public ResponseEntity<ProductResponse> addRawMaterial (@Valid @RequestBody ProductRawMaterialRequest request, @PathVariable Long id) {
+        return ResponseEntity.ok(productService.addRawMaterials(id, request));
+    }
+
+    @PutMapping("/{id}/raw-materials/{rawMaterialId}")
+    public ResponseEntity<ProductResponse> updateRequiredQuantity (@PathVariable Long id, @PathVariable Long rawMaterialId, @Valid @RequestBody UpdateRequiredQuantityRequest request) {
+        return ResponseEntity.ok(productService.updateRawMaterialQuantity(id, rawMaterialId, request));
+    }
+
+    @DeleteMapping("/{id}/raw-materials/{rawMaterialId}")
+    public ResponseEntity<ProductResponse> removeRawMaterial (@PathVariable Long id, @PathVariable Long rawMaterialId) {
+        return ResponseEntity.ok(productService.removeRawMaterial(id, rawMaterialId));
+    }
+
 }
