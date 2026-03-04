e57dcc7f6fe2d6ed9eb6df2197e740ef94f3a847
Tiago <tiagosune1@hotmail.com>
2026-03-04 16:18:44 -0300

    feat: add `@Data` annotation to production DTOs
    - Introduce Lombok's `@Data` annotation to `ProductProductionResponse` and `ProductionPlanResponse` for generating boilerplate code.

--- src/main/java/com/tiagosune/inventory_api/dto/production/ProductProductionResponse.java
+++ src/main/java/com/tiagosune/inventory_api/dto/production/ProductProductionResponse.java
@@ -1,10 +1,12 @@
 package com.tiagosune.inventory_api.dto.production;
 
 import lombok.Builder;
+import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Builder
+@Data
 public class ProductProductionResponse {
 
     private Long productId;
--- src/main/java/com/tiagosune/inventory_api/dto/production/ProductionPlanResponse.java
+++ src/main/java/com/tiagosune/inventory_api/dto/production/ProductionPlanResponse.java
@@ -1,11 +1,13 @@
 package com.tiagosune.inventory_api.dto.production;
 
 import lombok.Builder;
+import lombok.Data;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 @Builder
+@Data
 public class ProductionPlanResponse {
 
     private List<ProductProductionResponse> products;
