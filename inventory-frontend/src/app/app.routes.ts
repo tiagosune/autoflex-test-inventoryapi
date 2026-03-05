3fc45fcb2fb0b77bc39a7d3e849ec14eeb64744c
Tiago <tiagosune1@hotmail.com>
2026-03-05 02:54:19 -0300

    feat: add route for `RawMaterialsComponent` as default path

--- inventory-frontend/src/app/app.routes.ts
+++ inventory-frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { RawMaterialsComponent } from './features/raw-materials/raw-materials.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '',
+    component: RawMaterialsComponent
+  },
+];
